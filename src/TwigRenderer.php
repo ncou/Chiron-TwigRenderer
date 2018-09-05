c42489df82179a9c5442948a490db7f0365dcb93
ncou <nicolas_courade@yahoo.fr>
2018-09-05 14:52:38 +0000

    Apply fixes from StyleCI

--- src/TwigRenderer.php
+++ src/TwigRenderer.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Chiron\Views;
 
-use Chiron\Views\AttributesTrait;
-use Chiron\Views\TemplateRendererInterface;
-use Chiron\Views\TemplatePath;
 use Twig_Environment as TwigEnvironment;
 use Twig_Loader_Filesystem as TwigFilesystem;
-use LogicException;
 
 class TwigRenderer implements TemplateRendererInterface
 {
@@ -19,10 +15,12 @@ class TwigRenderer implements TemplateRendererInterface
      * @var string
      */
     private $suffix;
+
     /**
      * @var TwigFilesystem
      */
     protected $loader;
+
     /**
      * @var TwigEnvironment
      */
@@ -30,48 +28,53 @@ class TwigRenderer implements TemplateRendererInterface
 
     public function __construct(TwigEnvironment $engine = null, string $suffix = 'html')
     {
-        $this->engine   = $engine ?: $this->createEngine();
+        $this->engine = $engine ?: $this->createEngine();
         $this->loader = $this->engine->getLoader();
-        $this->suffix     = is_string($suffix) ? $suffix : 'html';
+        $this->suffix = is_string($suffix) ? $suffix : 'html';
     }
+
     /**
-     * Create a default Twig environment
+     * Create a default Twig environment.
      */
-    private function createEngine() : TwigEnvironment
+    private function createEngine(): TwigEnvironment
     {
         $loader = new TwigFilesystem();
+
         return new TwigEnvironment($loader);
     }
+
     /**
-     * Render
+     * Render.
      *
      * @param string $name
-     * @param array $params
+     * @param array  $params
      */
-    public function render(string $name, array $params = []) : string
+    public function render(string $name, array $params = []): string
     {
         // Merge parameters based on requested template name
         //$params = $this->mergeParams($name, $this->normalizeParams($params));
         $params = array_merge($this->attributes, $params);
-        $name   = $this->normalizeTemplate($name);
+        $name = $this->normalizeTemplate($name);
         // Merge parameters based on normalized template name
         //$params = $this->mergeParams($name, $params);
         return $this->engine->render($name, $params);
     }
+
     /**
-     * Add a path for template
+     * Add a path for template.
      */
-    public function addPath(string $path, string $namespace = null) : void
+    public function addPath(string $path, string $namespace = null): void
     {
         $namespace = $namespace ?: TwigFilesystem::MAIN_NAMESPACE;
         $this->loader->addPath($path, $namespace);
     }
+
     /**
-     * Get the template directories
+     * Get the template directories.
      *
      * @return TemplatePath[]
      */
-    public function getPaths() : array
+    public function getPaths(): array
     {
         $paths = [];
         foreach ($this->loader->getNamespaces() as $namespace) {
@@ -80,19 +83,21 @@ class TwigRenderer implements TemplateRendererInterface
                 $paths[] = new TemplatePath($path, $name);
             }
         }
+
         return $paths;
     }
 
     /**
-     * Checks if the view exists
+     * Checks if the view exists.
      *
-     * @param   string  $name  Full template path or part of a template path
+     * @param string $name Full template path or part of a template path
      *
-     * @return  boolean  True if the path exists
+     * @return bool True if the path exists
      */
     public function exists(string $name): bool
     {
-        $name   = $this->normalizeTemplate($name);
+        $name = $this->normalizeTemplate($name);
+
         return $this->loader->exists($name);
     }
 
@@ -101,12 +106,13 @@ class TwigRenderer implements TemplateRendererInterface
      *
      * Normalizes templates in the format "namespace::template" to "@namespace/template".
      */
-    public function normalizeTemplate(string $template) : string
+    public function normalizeTemplate(string $template): string
     {
         $template = preg_replace('#^([^:]+)::(.*)$#', '@$1/$2', $template);
         if (! preg_match('#\.[a-z]+$#i', $template)) {
             return sprintf('%s.%s', $template, $this->suffix);
         }
+
         return $template;
     }
 }
